78dfbfffb2add4b9698ba58e3aadf82b679fa51e
Anubhav Gang <87535773+anubhav-gang@users.noreply.github.com>
2023-04-23 18:24:59 +0530

    Add files via upload

---
+++ Sending WhatsApp Message .py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pywhatkit as Whatsapp
+
+
+# In[2]:
+
+
+Whatsapp.sendwhatmsg('+9194*****64','Hy, Hope you are doing great', 18,18)
+
+
+# In[ ]:
+
+
+
+
